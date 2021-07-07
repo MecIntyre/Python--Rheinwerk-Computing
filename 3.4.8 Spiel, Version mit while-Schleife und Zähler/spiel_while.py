14da51320788553879bcbc584d1ee96f6e7795f5
MecIntyre <abuesswalker@gmail.com>
2021-07-07 18:06:15 +0200

    neue Beispielaufgaben und Übungen gelöst

---
+++ 3.4.8 Spiel, Version mit while-Schleife und Zähler/spiel_while.py
@@ -0,0 +1,37 @@
+# Zufallsgenerator
+import random
+random.seed()
+
+# Werte und Berechnung 
+a = random.randint(1,10)
+b = random.randint(1,10)
+c = a + b
+print ("Die Aufgabe:", a, "+", b)
+
+# Schleife initialisieren
+zahl = c + 1
+
+# Anzahl initialisieren
+versuch = 0
+
+# while-Schleife
+while zahl != c:
+    # Anzahl Versuche
+    versuch = versuch + 1
+
+    # Eingabe mit Umwandlung
+    print("Bitte eine Zahl eingeben:")
+    z = input()
+    zahl = int(z)
+
+    # Verzweigung
+    if zahl == c:
+        print(zahl, "ist richtig")
+        # Abbruch der Schleife
+        break
+    else:
+        print(zahl, "ist falsch:")
+
+# Anzahl ausgeben 
+print("Ergebnis:", c)
+print("Anzahl Versuche:", versuch)
