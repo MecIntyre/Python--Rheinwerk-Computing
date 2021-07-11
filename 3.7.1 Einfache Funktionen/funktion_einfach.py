09f03a3d2606d2567752738e6142b1e1d274b9fd
Stephan Thiele <abuesswalker@gmail.com>
2021-07-11 15:10:19 +0200

    Einfache Funktionen hinzugefügt

---
+++ 3.7.1 Einfache Funktionen/funktion_einfach.py
@@ -0,0 +1,17 @@
+# Definition der FUnktion
+def stern():
+    print("_________________")
+    print("*** Trennung ****")
+    print("_________________")
+
+# Hauptprogramm
+x = 12
+y = 5
+
+stern()                         # 1. Aufruf
+print("x =", x, ", y =", y)
+stern()                         # 2. Aufruf
+print("x + y =", x + y)
+stern()                         # 3. Aufruf
+print("x - y =", x - y)
+stern()                         # 4. Aufruf
