62f0795b585b52425dafe083fcb4a895e73b3e91
Stephan Thiele <abuesswalker@gmail.com>
2021-07-20 11:12:27 +0200

    Schleifenbeispiele hinzugefügt

---
+++ Playground/1.4. Schleifen/quadrat_auflisten.py
@@ -0,0 +1,5 @@
+n = 10
+
+while n > 0:
+    print("Quadrat von", n, "ist", n ** 2)
+    n -= 1      # oder n = n - 1 
---
+++ Playground/1.4. Schleifen/while_schleife.py
@@ -0,0 +1,10 @@
+while 1 == 1:
+    eingabe = input("$ ")
+    print(eingabe)
+    if eingabe == "#":
+        continue
+    if eingabe == "":
+        break
+    print("Schleifenende")
+    
+print("Programmende")
