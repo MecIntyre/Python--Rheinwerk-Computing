fce0cf0e61db77e258d8f31b43153564095c97a2
Stephan Thiele <abuesswalker@gmail.com>
2021-07-21 12:56:21 +0200

    erste Funktion

---
+++ Playground/1.5 Funktionen/erste_funktion.py
@@ -0,0 +1,15 @@
+# Funktion 1
+def printHello():
+    print("Hallo ich bin eine funktionierende Funktion! :D")
+
+# Funktion 2 mit Parameter und Rückgabe
+def berechneQuadrat(zahl):
+    quadrat = zahl * zahl
+    return quadrat
+
+
+# Aufruf Funktion 1
+printHello()
+
+# Aufruf Funktion 2 mit Argument
+print(berechneQuadrat(9))
