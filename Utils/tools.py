9c792e774d50073c45181d71bc8676edd4e30fef
Stephan Thiele <abuesswalker@gmail.com>
2021-07-22 12:48:06 +0200

    add tuple example

--- Utils/tools.py
+++ Utils/tools.py
@@ -13,5 +13,13 @@ def showChoice(choiceOptions):
     for index, option in enumerate(choiceOptions):
         print(index + 1, ": ", option, sep="")
 
+def calculateSumAndProduct(a, b):
+    sum = a + b
+    product = a * b
+    return (sum, product)   # Ausgabe als tuple
+
+x, y = calculateSumAndProduct(2, 4) # Speichern der Argumente des Tuples direkt in x und y
+print(x, y)
+
 showChoice(["Stephan", "Mathias", "Christian", "Dyea", "Norbert"])
 readInput("Mögliche Eingabe: (1 - 5): ", ["1", "2", "3", "4", "5"])
