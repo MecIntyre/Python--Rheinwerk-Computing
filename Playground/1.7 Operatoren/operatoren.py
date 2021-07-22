b6bc9ce077ec370009b7fb861a819abba48c83ff
Stephan Thiele <abuesswalker@gmail.com>
2021-07-22 10:36:12 +0200

    not + comprehension hinzugefügt

--- Playground/1.7 Operatoren/operatoren.py
+++ Playground/1.7 Operatoren/operatoren.py
@@ -1,4 +1,4 @@
-namen = [
+namensliste = [
     "stephan",
     "mathias",
     "christian",
@@ -8,20 +8,26 @@ namen = [
     ]
 
 # Beispiel 1
-# Anzeige der Namen die auf "n" oder "s" enden.
+# Anzeige der Namen die nicht auf "n" oder "s" enden.
 
-for name in namen:
-    if (name[-1] == "n") or (name[-1] == "s"):
+gefilterteNamen = []
+for name in namensliste:
+    if not (name[-1] == "n") or (name[-1] == "s"):
         print(name)
-    #if name[-1] == "s":
-    #    print(name)
-
+        gefilterteNamen.append(name)
+    
 print()
 
 # Beispiel 2
+# List Comprehension (Vereinfachung der Darstellung)
+# Anzeige der Namen die auf "n" oder "s" enden
+gefilterteNamen = [name for name in namensliste if name[-1] in "sn"]
+
+
+# Beispiel 3
 # Amzeige der Namen die mit einem "s" anfange u
 # und ein "a" als vorletzten Buchstaben haben.
 
-for name in namen:
+for name in namensliste:
     if name[0] == "s" and name[-2] == "a":
         print(name)
