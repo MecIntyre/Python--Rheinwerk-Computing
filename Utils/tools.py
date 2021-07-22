f0aa070c8ac4ece4c58004cad9f9104c37a769d3
Stephan Thiele <abuesswalker@gmail.com>
2021-07-22 09:59:37 +0200

    Hilfsfunktionen (tools) hinzugfügt

---
+++ Utils/tools.py
@@ -0,0 +1,17 @@
+# Lass den Nutzer solange eine Eingabe tätigen, 
+# bis sie eine der vorgegebenen Möglichkeiten entspricht.
+def readInput(prompt, inputChoice):
+    while True:
+        choice = input(prompt)
+        choice = choice.lower()
+        if choice in inputChoice:
+            return choice
+
+# Ausgabe einer Liste der Auswahlmöglichkeiten.
+def showChoice(choiceOptions):
+    print("Das ist die Auswahl:")
+    for index, option in enumerate(choiceOptions):
+        print(index + 1, ": ", option, sep="")
+
+showChoice(["Stephan", "Mathias", "Christian", "Dyea", "Norbert"])
+readInput("Mögliche Eingabe: (1 - 5): ", ["1", "2", "3", "4", "5"])
