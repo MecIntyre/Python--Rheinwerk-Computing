65895ef3ddbe055e42f976e32bd09d7762506ecc
Stephan Thiele <abuesswalker@gmail.com>
2021-07-29 08:48:42 +0200

    Syntaxänderung

--- Exercises/14u_fizzbuzz.py
+++ Exercises/14u_fizzbuzz.py
@@ -4,8 +4,6 @@
 # ist die Zahl durch 3 und 5 teilbar, gibt das Programm "FizzBuzz" aus
 # andernfalls wird die Zahl ausgegeben
 
-input = int(input("Bitte gib eine Zahl von 1 bis 100 ein: "))
-
 for input in range(1,101):
     if input % 3 == 0 and input % 5== 0:
         print ("FizzBuzz")
